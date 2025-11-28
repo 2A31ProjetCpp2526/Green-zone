28d6ff72bb8d2134580f63257dcc1a63a03e4ceb
Shaima1234A <ShaimaAbdulMalikBEST1@gmail.com>
2025-11-28 19:28:27 +0100

    Implement main function for application startup
    Initialize main application and show main window with chart dialog.

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "connection.h"
+#include <QApplication>
+#include "chartdialog.h"
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Connection *c = Connection::getInstance();
+    if (!c->openConnection()) return -1;
+
+    MainWindow w;
+    w.show();
+    ChartDialog dlg;   // ★ SHOW CHART
+    dlg.exec();
+
+
+    return a.exec();
+}
